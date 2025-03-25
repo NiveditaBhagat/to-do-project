1dbde9a63245d8df473d962bcdae1f572c5acbeb
Nivedita Bhagat <bhagatnivedita469@gmail.com>
2025-03-25 23:47:42 +0530

    user table creation

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base # for creating database object
 
-SQLALCHEMY_DATABASE_URL='sqlite:///./todos.db' 
+SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db' 
 # This is url is going to be used to create the location of this database on our project.
 #  Create an engine for our application. Database engine is something that we can use to able to open up a connection and be able to use our database.
 
@@ -12,6 +12,8 @@ engine=create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':
 # This is to prevent any kind of accident sharing of the same connection for different kind of request.
 # But in FASTAPI it is very normal to have more than one thread that could interact with the database at the same time.
 # Now we need to create a session local and each instance of the session local will have a database session.
+
+# sqlalchemy cannot enhance a table for us, it can only create for us
 
 SessionLocal= sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- models.py
+++ models.py
@@ -1,7 +1,19 @@
 # Model is a way for SQL Alchemy is to be able to understand what kind of databse tables we are going to create.
 # Now a database model is going to be the actual record that is inside the database table.
 from database import Base
-from sqlalchemy import Column, Integer, String,Boolean
+from sqlalchemy import Column, Integer, String,Boolean, ForeignKey
+
+
+class Users(Base):
+     __tablename__='users'
+     id=Column(Integer,primary_key=True,index=True)
+     email=Column(String, unique=True)
+     username=Column(String, unique=True)
+     first_name=Column(String)
+     last_name=Column(String)
+     hashed_password= Column(String)
+     is_active= Column(Boolean, default=True)
+     role= Column(String)
 
 class Todos(Base):
     __tablename__='todos' # now this is just a way for SQL alchemy to know what to name this table inside our database later on.
@@ -12,5 +24,6 @@ class Todos(Base):
     description=Column(String)
     priority=Column(Integer)
     complete=Column(Boolean, default=False)
+    ownner_id= Column(Integer,ForeignKey("users.id"))
     
 
