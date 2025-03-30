16b4cfdae3b0ba09c20c6f0b625be8211162b012
Nivedita Bhagat <bhagatnivedita469@gmail.com>
2025-03-30 23:01:28 +0530

    Authenticate user

--- routers/auth.py
+++ routers/auth.py
@@ -7,6 +7,7 @@ from passlib.context import CryptContext
 from database import SessionLocal
 from starlette import status
 from typing import Annotated
+from fastapi.security import OAuth2PasswordRequestForm
 
 router=APIRouter()
 
@@ -32,6 +33,16 @@ def get_db():
 
 db_dependency=Annotated[Session, Depends(get_db)]
 
+def authenticate_user(username: str, password: str, db):
+    user= db.query(Users).filter(Users.username==username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, Users.hashed_password):
+        return False
+    return True
+
+
+
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db:db_dependency, 
@@ -47,3 +58,11 @@ async def create_user(db:db_dependency,
     )
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_acces_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user= authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed authentication'
+    return 'Succesful authenticationken'
