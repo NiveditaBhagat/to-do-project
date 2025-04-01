03536e572a5435d106155277599c54a11e785f58
Nivedita Bhagat <bhagatnivedita469@gmail.com>
2025-04-01 23:21:47 +0530

    decode JWT

--- routers/auth.py
+++ routers/auth.py
@@ -2,14 +2,14 @@ from datetime import datetime, timedelta, timezone
 from fastapi import  APIRouter
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,HTTPException
 from models import Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from starlette import status
 from typing import Annotated
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
+from jose import jwt, JWTError
 
 router=APIRouter()
 
@@ -19,6 +19,8 @@ ALGORITHM='HS256'
 
 #bcrypt_context= CryptContext(schemes=['bcrypt'], deprecated='auto')
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer= OAuth2PasswordBearer(tokenUrl='token')
+
 
 
 class CreateUserRequest(BaseModel):
@@ -64,6 +66,21 @@ def create_access_token(username: str, user_id: int, expires_delta:timedelta):
     expires=datetime.now(timezone.utc) + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str =payload.get('sub')
+        user_id: str=payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.'
+                                )
+        return {'username': username, 'id':user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.'
+                                )
 
 
 
