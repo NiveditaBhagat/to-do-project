5d3c73aa8442d4377a07770c41ec722545fcc2bf
Nivedita Bhagat <bhagatnivedita469@gmail.com>
2025-04-06 23:23:07 +0530

    todos

--- routers/auth.py
+++ routers/auth.py
@@ -109,9 +109,6 @@ async def create_user(db:db_dependency,
     db.add(create_user_model)
     db.commit()
 
-
-
-
 @router.post("/token", response_model=Token)
 async def login_for_acces_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user= authenticate_user(form_data.username, form_data.password, db)
