ca84492a872c5835c693c3b94e82ce80bdfd0201
Nivedita Bhagat <bhagatnivedita469@gmail.com>
2025-04-09 23:20:21 +0530

    Put Todo(User ID)

--- routers/todos.py
+++ routers/todos.py
@@ -66,11 +66,13 @@ async def create_todo(user: user_dependency, db:db_dependency,todo_request: Todo
 
 
 @router.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db: db_dependency,
+async def update_todo(user: user_dependency, db: db_dependency,
                       todo_request: TodoRequest,
                       todo_id: int =Path(gt=0)
                       ):
-    todo_model= db.query(Todos).filter(todo_id==Todos.id).first()
+    if user is None:
+        raise HTTPException(status_code=401,detail='Authentication failed')
+    todo_model= db.query(Todos).filter(todo_id==Todos.id).filter(Todos.ownner_id==user.get('id')).first()
     if todo_model is None:
         raise HTTPException(status_code=404,detail='Todo not found.')
     
