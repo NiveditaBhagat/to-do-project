a0df39eefd89550738ed36664a1a4b539015e99c
Nivedita Bhagat <bhagatnivedita469@gmail.com>
2025-04-15 19:46:01 +0530

    users router

--- routers/users.py
+++ routers/users.py
@@ -36,13 +36,27 @@ db_dependency=Annotated[Session, Depends(get_db)]
 user_dependency= Annotated[dict, Depends(get_current_user)]
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+class UserVerification(BaseModel):
+    password:str
+    new_password: str=Field(min_length=6)
 
 
-@router.get("/users", status_code=status.HTTP_200_OK)
+
+@router.get("/", status_code=status.HTTP_200_OK)
 async def get_users(db: db_dependency, user: user_dependency):
     if user is None :
         raise HTTPException(status_code=404, detail='User not found')
     return db.query(Todos).filter(Users.id==user.get('id')).first()
 
-
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(db: db_dependency, user: user_dependency, user_verificartion: UserVerification ):
+    if user is None:
+        raise HTTPException(status_code=404, detail='Authentication Failed')
+    user_model= db.query(Users).filter(Users.id==user.get('id')).first()
+    
+    if not bcrypt_context.verify(user_verificartion.password,user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password change')
+    user_model.hashed_password = bcrypt_context(user_verificartion.new_password)
+    db.add(user_model)
+    db.commit()
     
