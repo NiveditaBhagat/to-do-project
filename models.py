62e6eee2345db9cd014bb137c7efbed7a0e41b7e
Nivedita Bhagat <bhagatnivedita469@gmail.com>
2025-05-03 22:50:23 +0530

    commit

--- models.py
+++ models.py
@@ -4,6 +4,10 @@ from database import Base
 from sqlalchemy import Column, Integer, String,Boolean, ForeignKey
 
 
+
+
+
+
 class Users(Base):
      __tablename__='users'
      id=Column(Integer,primary_key=True,index=True)
